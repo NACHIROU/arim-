6e1f0371b5a699c4badf16f614d54afe6c79b6cc
NACHIROU <nachirousomad24@gmail.com>
2025-07-22 17:35:25 +0100

    cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,7 +23,7 @@ app.include_router(dashboard.router, prefix="/dashboard", tags=["Dashboard"])
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
